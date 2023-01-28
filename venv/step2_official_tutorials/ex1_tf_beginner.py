79416e0f786eb3db94abe17175b0a653974f3862
WangGaige <wanggaige21@126.com>
2023-01-29 00:00:53 +0800

    ex1_upload

--- venv/step2_official_tutorials/ex1_tf_beginner.py
+++ venv/step2_official_tutorials/ex1_tf_beginner.py
@@ -1,9 +1,27 @@
 #TensorFlow 2 quickstart for beginners
 
 # Set up TensorFlow
-
+import tensorflow as tf
+import tensorflow.keras as keras
 # Load a dataset
+mnist=tf.keras.datasets.mnist
+(x_train,y_train),(x_test,y_test)=mnist.load_data()
+x_train,x_test=x_train/255, x_test/255
 
 # Build a machine learning model
+model=tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28,28)),
+    tf.keras.layers.Dense(128,activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10)
+])
 
+loss_fn=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+model.compile(optimizer='adam',loss=loss_fn,metrics=['accuracy'])
 # Train and evaluate your model
+model.fit(x_train,y_train,epochs=5)
+
+a=model.evaluate(x_test,y_test,verbose=2)
+
+print(a)
